cad61616f55cfbaca3476ab6ca372424baf57e90
NITHISH KUMAR M <123449276+NITHISH0403@users.noreply.github.com>
2023-02-28 13:41:36 +0530

    Add files via upload

---
+++ src/main/java/org/example/Payment.java
@@ -0,0 +1,36 @@
+package org.example;
+
+import java.util.logging.Logger;
+
+class Payment
+{
+    static Logger l=Logger.getLogger("My Logger");
+
+    //constructor
+    Payment (String name, int no)
+    {
+        l.info ("Hii\t" + name + "\tyour account will be created.");
+        l.info ("Your account No is\t" + no);
+    }
+
+    //method that allows the user to make a deposit to the account.
+    double deposit (double d, double balance)
+    {
+        balance = balance + d;
+        return balance;
+    }
+
+    //method that allows the user to make a Withdrawal to the account.
+    double withdraw (double w, double balance)
+    {
+        balance = balance - w;
+        return balance;
+    }
+
+    //method that returns the current balance of the account.
+    void amount (double balance)
+    {
+        l.info ("Your Current Balance :" + balance);
+    }
+
+}
